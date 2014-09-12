ab7fd02f7064824ae7115affb6b62fe06bbfe93b
Elena <elena.15@hotmail.it>
2014-09-12 16:36:37 +0100

    added iterator

--- directory.rb
+++ directory.rb
@@ -8,11 +8,9 @@ students = [
 #and then print them
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
+students.each do |student|
+   puts student
+end
 #finally, we print the total
 print "Overall, we have #{students.length} great students"
 #it's important that print() doesn't add new line characters
