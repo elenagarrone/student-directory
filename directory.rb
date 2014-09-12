2f86113c107054ce6ca4580529fab622938ab5c7
Elena <elena.15@hotmail.it>
2014-09-12 16:22:30 +0100

    added array and added .length

--- directory.rb
+++ directory.rb
@@ -1,12 +1,18 @@
-student_count = 5
-#first we print the list of students 
+#let's put all students into an array
+students = [
+"Hadi",
+"Karin",
+"James",
+"Sean",
+"Fadie"]
+#and then print them
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
-puts "Hadi"
-puts "Karin"
-puts "James"
-puts "Sean"
-puts "Fadie"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
 #finally, we print the total
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.length} great students"
 #it's important that print() doesn't add new line characters
