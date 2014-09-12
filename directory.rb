611ac4caf7146128c4b1289a1a8aabba194e9748
Elena <elena.15@hotmail.it>
2014-09-12 16:04:45 +0100

    added some comments to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students 
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
 puts "Hadi"
@@ -5,6 +6,8 @@ puts "Karin"
 puts "James"
 puts "Sean"
 puts "Fadie"
+#finally, we print the total
 print "Overall, we have "
+#it's important that print() doesn't add new line characters
 print 5
 puts " great students"
