c8d43b605eb2fe30d8f20a501695b58bb4f77b7e
Elena <elena.15@hotmail.it>
2014-09-13 18:53:37 +0100

    added interactive menu and refacted the code

--- directory.rb
+++ directory.rb
@@ -1,38 +1,67 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
 puts "Please enter the name of the students"
 puts "To finish, just hit return twice"
-#create an empty arrays
-students = []
 #get the first name
 name = gets.chomp
 #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-	students << {:name => name, :cohort => :november}
-	puts "Now we have #{students.length} students"
+	@students << {:name => name, :cohort => :november}
+	puts "Now we have #{@students.length} students"
 	name = gets.chomp
   end
-  #return the array of students
-  students
-end
+ end
 
 def print_header
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-  puts "#{1+index}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each_with_index do |student, index|
+  puts "#{1+index} #{student[:name]} (#{student[:cohort]} cohort)"       
   end
 end
 
-def print_footer(names)
-puts "Overall, we have #{names.length} great students"
+def print_footer
+puts "Overall, we have #{@students.length} great students"
 end
-#nothing happens until we call the methods
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+   loop do #we need to repeat this action a number of times
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+      when "1"
+      #input the students
+      input_students
+      when "2"
+      #show the students
+      show_students
+      when "9" 
+      exit #this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+  end
+end
+
+#nothing happens until we call the methods
+interactive_menu
