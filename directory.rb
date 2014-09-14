5b005eb7d91e296fa9862787a26fd7389c2bed25
Elena <elena.15@hotmail.it>
2014-09-14 12:45:13 +0100

    added loading the data from a file

--- directory.rb
+++ directory.rb
@@ -40,6 +40,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" #we'll be adding more items
 end
 
@@ -59,6 +60,8 @@ def process(selection)
       show_students
       when "3"
       save_students
+      when "4"
+      load_students
       when "9" 
       exit #this will cause the program to terminate
       else
@@ -77,5 +80,15 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv","r")  
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+     @students << {:name => name, :cohort => cohort.to_sym}
+  end
+file.close
+end
+
 #nothing happens until we call the methods
 interactive_menu
