1341d545fc0ea48c312259eba1512d36baf47832
Elena <elena.15@hotmail.it>
2014-12-10 12:28:32 +0000

    exercise8 completed

---
+++ exercise8.rb
@@ -0,0 +1,42 @@
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  #create an empty arrays
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "In which cohort are you in?"
+    cohort = gets.chomp
+    puts "Can i ask your date of birth?"
+    birth = gets.chomp
+    #add the student hash to the array
+    students << {:name => name, :cohort => cohort, :birth => birth}
+    puts "Now we have #{students.length} students\n"
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "--------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort. Date of birth: #{student[:birth]})"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students\n"
+end
+#nothing happens until we call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
