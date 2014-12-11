15b8d1a4e4805cee21ba4c80668c7b430ea6f269
Elena <elena.15@hotmail.it>
2014-12-11 14:33:43 +0000

    refactored exercise8

--- exercise8.rb
+++ exercise8.rb
@@ -1,29 +1,23 @@
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  #create an empty arrays
   students = []
-  #get the first name
   name = gets.chomp
-  #while the name is not empty, repeat this code
   while !name.empty? do
     puts "In which cohort are you in?"
     cohort = gets.chomp.capitalize
     cohort == "" ? cohort=:September : ""
     cohort = check_spelling(cohort)
-
     puts "Can i ask your date of birth?"
     birth = gets.chomp
-    #add the student hash to the array
     students << {:name => name, :cohort => cohort, :birth => birth}
     if students.length == 1
-      print "Now we have #{students.length} student\n"
+      puts "Now we have #{students.length} student"
     else
-      print "Now we have #{students.length} students\n"
+      puts "Now we have #{students.length} students"
     end
     name = gets.chomp
   end
-  #return the array of students
   students
 end
 
@@ -35,7 +29,6 @@ def check_spelling(cohort)
     re_entering_cohort = gets.chomp.capitalize
     return check_spelling(re_entering_cohort)
   end
-
 end
 
 def print_header
@@ -50,9 +43,8 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students\n"
+  puts "Overall, we have #{names.length} great students"
 end
-#nothing happens until we call the methods
 
 students = input_students
 print_header
