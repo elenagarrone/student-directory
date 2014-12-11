907d1b28e1092db237a30f625188453eb890acb0
Elena <elena.15@hotmail.it>
2014-12-11 14:10:51 +0000

    refactored exercise1

--- exercise1.rb
+++ exercise1.rb
@@ -1,34 +1,28 @@
 def input_students
-print "Please enter the name of the students. \nTo finish, just hit return twice. \n"
-#create an empty arrays
-students = []
-#get the first name
-name = gets.chomp
-#while the name is not empty, repeat this code
+  print "Please enter the name of the students. \nTo finish, just hit return twice. \n"
+  students = []
+  name = gets.chomp
   while !name.empty? do
-    #add the student hash to the array
-	students << {:name => name, :cohort => :november}
-	print "Now we have #{students.length} students\n"
-	name = gets.chomp
+  	students << {:name => name, :cohort => :november}
+  	puts "Now we have #{students.length} students"
+  	name = gets.chomp
   end
-  #return the array of students
   students
 end
 
 def print_header
-print "The students of my cohort at Makers Academy \n--------------\n"
+  puts "The students of my cohort at Makers Academy \n--------------"
 end
 
 def print_students(students)
   students.each do |student|
-  print "#{student[:name]} (#{student[:cohort]} cohort)\n"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-print "Overall, we have #{names.length} great students\n"
+  puts "Overall, we have #{names.length} great students"
 end
-#nothing happens until we call the methods
 
 students = input_students
 print_header
