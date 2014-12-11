5c722dea149d06b03b8d41ca6f18a6020a06b3ef
Elena <elena.15@hotmail.it>
2014-12-11 14:44:44 +0000

    refactores exercise12

--- exercise12.rb
+++ exercise12.rb
@@ -10,25 +10,19 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students\n"
+  puts "Overall, we have #{names.length} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
   students = []
-  #get the first name
   name = gets.chomp
-  #while the name is not empty, repeat this code
   while !name.empty? do
-    #add the student hash to the array
     students << {:name => name, :cohort => :november}
-    puts "Now we have #{students.length} students\n"
-    #get another name from the user
+    puts "Now we have #{students.length} students"
     name = gets.chomp
   end
-  #return the array of students
   students
 end
 
