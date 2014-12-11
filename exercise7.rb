737bf347f2f1c6414a05ffee0cb8de483f220f6f
Elena <elena.15@hotmail.it>
2014-12-11 14:29:19 +0000

    refactored exercise7

--- exercise7.rb
+++ exercise7.rb
@@ -1,38 +1,32 @@
 def input_students
-puts "Please enter the name of the students".center(40)
-puts "To finish, just hit return twice".center(40)
-#create an empty arrays
-students = []
-#get the first name
-name = gets.chomp
-#while the name is not empty, repeat this code
+  puts "Please enter the name of the students".center(40)
+  puts "To finish, just hit return twice".center(40)
+  students = []
+  name = gets.chomp
   while !name.empty? do
     puts "Which cohort?".center(40)
     cohort = gets.chomp
-    #add the student hash to the array
-  students << {:name => name, :cohort => cohort}
-  puts "Now we have #{students.length} students\n".center(40)
-  name = gets.chomp
+    students << {:name => name, :cohort => cohort}
+    puts "Now we have #{students.length} students".center(40)
+    name = gets.chomp
   end
-  #return the array of students
   students
 end
 
 def print_header
-puts "The students of my cohort at Makers Academy".center(40)
-puts "--------------".center(40)
+  puts "The students of my cohort at Makers Academy".center(40)
+  puts "--------------".center(40)
 end
 
 def print(students)
   students.each do |student, cohort|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
   end
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.length} great students\n".center(40)
+  puts "Overall, we have #{names.length} great students".center(40)
 end
-#nothing happens until we call the methods
 
 students = input_students
 print_header
